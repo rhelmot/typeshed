daa9373b60042c1ca5f0f0ef116e06db1ebeb695
Avasam <samuel.06@hotmail.com>
2023-10-22 20:40:27 -0400

    Run stubdefaulter on networkx

--- stubs/networkx/networkx/convert_matrix.pyi
+++ stubs/networkx/networkx/convert_matrix.pyi
@@ -9,38 +9,38 @@ from pandas.core.dtypes.base import ExtensionDtype
 
 def to_pandas_adjacency(
     G: Graph[_Node],
-    nodelist: list[_Node] | None = ...,
-    dtype: numpy.dtype[Incomplete] | None = ...,
-    order: Literal["C", "F"] | None = ...,
+    nodelist: list[_Node] | None = None,
+    dtype: numpy.dtype[Incomplete] | None = None,
+    order: Literal["C", "F"] | None = None,
     multigraph_weight: Callable[[Iterable[float]], float] = ...,
-    weight: str = ...,
-    nonedge: float = ...,
+    weight: str = "weight",
+    nonedge: float = 0.0,
 ) -> DataFrame: ...
-def from_pandas_adjacency(df: DataFrame, create_using: type[Graph[Incomplete]] = ...) -> Graph[Incomplete]: ...
+def from_pandas_adjacency(df: DataFrame, create_using: type[Graph[Incomplete]] | None = None) -> Graph[Incomplete]: ...
 def to_pandas_edgelist(
     G: Graph[_Node],
-    source: str | int = ...,
-    target: str | int = ...,
-    nodelist: list[_Node] | None = ...,
-    dtype: ExtensionDtype | None = ...,
-    edge_key: str | int | None = ...,
+    source: str | int = "source",
+    target: str | int = "target",
+    nodelist: list[_Node] | None = None,
+    dtype: ExtensionDtype | None = None,
+    edge_key: str | int | None = None,
 ) -> DataFrame: ...
 def from_pandas_edgelist(
     df: DataFrame,
-    source: str | int = ...,
-    target: str | int = ...,
-    edge_attr: str | int | Iterable[str | int] | Literal[True] | None = ...,
-    create_using: type[Graph[Incomplete]] = ...,
+    source: str | int = "source",
+    target: str | int = "target",
+    edge_attr: str | int | Iterable[str | int] | Literal[True] | None = None,
+    create_using: type[Graph[Incomplete]] | None = None,
 ) -> Graph[Incomplete]: ...
 def to_numpy_array(
     G: Graph[_Node],
-    nodelist: list[_Node] | None = ...,
-    dtype: numpy.dtype[Incomplete] | None = ...,
-    order: Literal["C", "F"] | None = ...,
+    nodelist: list[_Node] | None = None,
+    dtype: numpy.dtype[Incomplete] | None = None,
+    order: Literal["C", "F"] | None = None,
     multigraph_weight: Callable[[Iterable[float]], float] = ...,
-    weight: str = ...,
-    nonedge: float = ...,
+    weight: str = "weight",
+    nonedge: float = 0.0,
 ) -> numpy.ndarray[Incomplete, numpy.dtype[Incomplete]]: ...
 def from_numpy_array(
-    A: numpy.ndarray[Incomplete, Incomplete], parallel_edges: bool = ..., create_using: type[Graph[Incomplete]] = ...
+    A: numpy.ndarray[Incomplete, Incomplete], parallel_edges: bool = False, create_using: type[Graph[Incomplete]] | None = None
 ) -> Graph[Incomplete]: ...
